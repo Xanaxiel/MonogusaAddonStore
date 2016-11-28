389d304e61c2a24298c1d92deb1952a0cdf724e2
monogusa <nicorette.kinen@gmail.com>
2016-11-29 00:24:33 +0900

    indunplusβ版

--- indunplus/src/indunplus.lua
+++ indunplus/src/indunplus.lua
@@ -12,11 +12,23 @@ g.settingsFileLoc = "../addons/indunplus/settings.json";
 
 if not g.loaded then
 	g.isDragging = false;
+	g.color = {
+		normal = "FFFFFFFF",
+		nearComplete = "FF00FFFF",
+		complete = "FF00FF00",
+	};
 	g.settings = {
+		--表示非表示
 		show = false;
+		--X座標、Y座標
 		xPosition = 500,
 		yPosition = 500,
+		--リセット時刻
 		resetHour = 6,
+		--フォントのサイズ
+		fontSize = 16,
+		--1列に表示するキャラ数
+		rowMax = 5,
 		records = {},
 	};
 
@@ -26,28 +38,34 @@ if not g.loaded then
 		["100"] = {
 			id = "Indun_startower",
 			label = "ID",
+			level = 50,
 		},
 		["200"] = {
 			id = "Request_Mission1",
 			label = "傭兵依頼",
+			level = 100,
 		},
 		["300"] = {
 			id = "Request_Mission7",
 			label = "サルラス",
+			level = 240,
 		},
 		["400"] = {
 			id = "M_GTOWER_1",
 			label = "大地",
+			level = 260,
 		},
 		["500"] = {
 			id = "Request_Mission10",
 			label = "防衛",
+			level = 120,
 		},
 	};
 
 end
 
 function g.processCommand(words)
+	INDUNPLUS_SHOW_PLAYCOUNT();
 	INDUNPLUS_TOGGLE_FRAME();
 end
 
@@ -93,6 +111,39 @@ function g.getBossDebuff()
 	
 end
 
+function g.createCharaNameText(frame, cid, record, fontSize, x, y, width, height)
+	local charaText = frame:CreateOrGetControl("richtext", "record"..cid, x, y, width, height)
+	tolua.cast(charaText, "ui::CRichText");
+	
+	local text = string.format("{s%d}%s{/}", fontSize, record.name);
+	charaText:SetText(text);
+	charaText:ShowWindow(1);
+end
+
+function g.createFBTimeText(frame, cid, record, fontSize, x, y, width, height)
+	local g = _G['ADDONS']['MONOGUSA']['INDUNPLUS'];
+
+	local color = "FFFFFF";
+	if record.fbDebuffTime - 600 < os.time()  then
+		color = "00FF00";
+	elseif record.fbDebuffTime - 3600 < os.time() then
+		color = "FFFF00";
+	end
+
+	local fbLabelText = frame:CreateOrGetControl("richtext", "fbLabel"..cid, x, y, width, height)
+	local text = string.format("{#%s}{s%d}%s{/}{/}", color, fontSize, "FBデバフ");
+	tolua.cast(fbLabelText, "ui::CRichText");
+	fbLabelText:SetText(text);
+	fbLabelText:ShowWindow(1);
+
+	local fbText = frame:CreateOrGetControl("richtext", "fbDebuff"..cid, x, y, width, height)
+	local fbDate = os.date("*t", record.fbDebuffTime);
+	tolua.cast(fbText, "ui::CRichText");
+	fbText:SetText(string.format("{#%s}{s%d}%02d:%02d:%02d{/}{/}",color, fontSize, fbDate.hour, fbDate.min, fbDate.sec));
+	fbText:SetGravity(1,0);
+	fbText:ShowWindow(1);
+end
+
 function INDUNPLUS_TOGGLE_FRAME()
 	local g = _G['ADDONS']['MONOGUSA']['INDUNPLUS'];
 	local acutil = require('acutil');
@@ -108,51 +159,81 @@ function INDUNPLUS_SHOW_PLAYCOUNT()
 	local records = g.settings.records;
 	
 	local frame = ui.GetFrame("indunplus");
-	local height = -10;
-	
-	for cid, record in pairs(records) do
-		local line = record.name.." ";
+	local fontSize = g.settings.fontSize or 16
 
-		height = height + 20;
-		local charaText = frame:CreateOrGetControl("richtext", "record"..cid, 10, height, 200, 20);
-		tolua.cast(charaText, "ui::CRichText");
-		charaText:SetText("{s14}"..record.name);
-		charaText:ShowWindow(1);
-		
-		if nil ~= record.fbDebuffTime then
-			if record.fbDebuffTime > os.time() then
-				height = height + 15;
-				local fbLabelText = frame:CreateOrGetControl("richtext", "fbLabel"..cid, 20, height, 100, 15);
-				tolua.cast(fbLabelText, "ui::CRichText");
-				fbLabelText:SetText("{s14}".."FBデバフ");
-				fbLabelText:ShowWindow(1);
-			
-				local fbText = frame:CreateOrGetControl("richtext", "fbDebuff"..cid, -20, height, 100, 15);
-				local fbDate = os.date("*t", record.fbDebuffTime);
-				tolua.cast(fbText, "ui::CRichText");
-				fbText:SetText(string.format("{s14}".."%02d:%02d:%02d", fbDate.hour, fbDate.min, fbDate.sec));
-				fbText:SetGravity(1,0);
-				fbText:ShowWindow(1);
-			end
+	local topMargin = 15;
+	local bottomMargin = 15;
+	local width = 0;
+	local height = 0;
+	local cnt = 0;
+	local rowMax = g.settings.rowMax or 5;
+	
+	local row = 0;
+	local col = 0;
+	
+	local pageX = 0;
+	local pageY = 15;
+	local pageWidth = 200;
+	local pageHeight = fontSize * 7 + 10;
+
+	for cid, record in pairs(records) do
+
+		if cnt > 0 and cnt % rowMax == 0 then
+			row = 0;
+			height = 0;
+			col = col + 1;
+			pageX = pageWidth * col;
+		end
+
+		pageY = pageHeight * row + topMargin;
+		local page = frame:CreateOrGetControl("groupbox", "page_"..cid, pageX , pageY, pageWidth, pageHeight);
+		page:SetSkinName('None');
+
+		local y = 5;
+		g.createCharaNameText(page, cid, record, fontSize, 12, y, pageWidth, 20);
+
+		if nil ~= record.fbDebuffTime and  record.fbDebuffTime > os.time() then
+			y = y + fontSize;
+			g.createFBTimeText(page, cid, record, fontSize, 20, y, pageWidth, 20)
 		end
 
 		for type, counts in pairs(record.counts) do
-			height = height + 15;
+			y = y + fontSize;
+
 			local label = g.types[type].label;
-			
-			local labelText = frame:CreateOrGetControl("richtext", "label"..cid.."_"..type, 20, height, 100, 15);
+
+			local color = "FFFFFF";
+			if counts.playCount >= counts.maxPlayCount then
+				color = "00FF00";
+			elseif counts.playCount > 0 then
+				color = "FFFF00";
+			end
+
+			local labelText = page:CreateOrGetControl("richtext", "label"..cid.."_"..type, 20, y, pageWidth/2, 15);
 			tolua.cast(labelText, "ui::CRichText");
-			labelText:SetText("{s14}"..label);
+			labelText:SetText( string.format("{#%s}{s%d}%s{/}{/}", color, fontSize ,label));
 			labelText:ShowWindow(1);
 
-			local countText = frame:CreateOrGetControl("richtext", "count"..cid.."_"..type, -20, height, 100, 15);
+			local countText = page:CreateOrGetControl("richtext", "count"..cid.."_"..type, 0, y, pageWidth/2, 15);
 			tolua.cast(countText, "ui::CRichText");
-			countText:SetText("{s14}"..counts.playCount.."/"..counts.maxPlayCount);
+			countText:SetText( string.format("{#%s}{s%d}%d/%d{/}{/}", color, fontSize, counts.playCount, counts.maxPlayCount));
 			countText:SetGravity(1,0);
 			countText:ShowWindow(1);
 		end
+
+		page:Resize(pageWidth, pageHeight);
+
+		if cnt < rowMax then
+			height = pageHeight * (row + 1) + bottomMargin + topMargin;
+		else
+			height = pageHeight * rowMax + bottomMargin + topMargin;
+		end
+
+		cnt = cnt + 1;
+		row = row + 1;
 	end
-	frame:Resize(frame:GetWidth(),height + 30);
+	frame:Resize(pageWidth * (col + 1) + 10, height);
+
 end
 
 function INDUNPLUS_ON_INIT(addon, frame)
@@ -167,6 +248,7 @@ function INDUNPLUS_ON_INIT(addon, frame)
 	g.addon:RegisterMsg("GAME_START_3SEC", "INDUNPLUS_3SEC");
 	g.addon:RegisterMsg('BUFF_ADD', 'INDUNPLUS_UPDATE_BUFF');
 	g.addon:RegisterMsg('BUFF_REMOVE', 'INDUNPLUS_UPDATE_BUFF');
+
 end
 
 
@@ -255,6 +337,9 @@ function INDUNPLUS_3SEC()
 
 	local frame = ui.GetFrame("indunplus");
 
+	INDUNPLUS_SAVE_TIME();
+	INDUNPLUS_SHOW_PLAYCOUNT();
+
 	if frame ~= nil and not g.isDragging then
 		if g.settings.show then
 			frame:ShowWindow(1);
@@ -264,9 +349,6 @@ function INDUNPLUS_3SEC()
 		frame:Move(0, 0);
 		frame:SetOffset(g.settings.xPosition, g.settings.yPosition);
 	end
-
-	INDUNPLUS_SAVE_TIME();
-	INDUNPLUS_SHOW_PLAYCOUNT();
 end
 
 function INDUNPLUS_SAVE_TIME()
@@ -299,10 +381,6 @@ function INDUNPLUS_SAVE_TIME()
 			["maxPlayCount"] = maxPlayCount,
 		};
 	end
-
-	local frame = ui.GetFrame("indunplus");
-	g.settings.xPosition = frame:GetX();
-	g.settings.yPosition = frame:GetY();
 
 	acutil.saveJSON(g.settingsFileLoc, g.settings);
 end
