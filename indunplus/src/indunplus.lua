66b68e58110491be2d4457ef4a111e5a93d35885
Monogusa1244 <nicorette.kinen@gmail.com>
2016-12-09 20:58:29 +0900

    IndunPlus v1.1.0 release

--- indunplus/src/indunplus.lua
+++ indunplus/src/indunplus.lua
@@ -33,36 +33,37 @@ if not g.loaded then
 	};
 
 	g.bossDebuffId = 80001;
-
-	g.types = {
-		["100"] = {
-			id = "Indun_startower",
-			label = "ID",
-			level = 50,
-		},
-		["200"] = {
-			id = "Request_Mission1",
-			label = "傭兵依頼",
-			level = 100,
-		},
-		["300"] = {
-			id = "Request_Mission7",
-			label = "サルラス",
-			level = 240,
-		},
-		["400"] = {
-			id = "M_GTOWER_1",
-			label = "大地",
-			level = 260,
-		},
-		["500"] = {
-			id = "Request_Mission10",
-			label = "防衛",
-			level = 120,
-		},
-	};
-
 end
+
+function INDUNPLUS_GET_TYPES()
+	local clslist, cnt = GetClassList("Indun");
+  local temp = {};
+  local result = {};
+
+  local categoryCount = 1;
+	for i = 0 , cnt - 1 do
+ 		local cls = GetClassByIndexFromList(clslist, i);
+    local idx = temp[tostring(cls.PlayPerResetType)];
+    
+    if idx == nil and cls.Category ~= 'None' then
+      table.insert(result,
+        categoryCount,
+        {
+          ["type"] = tostring(cls.PlayPerResetType),
+          ["label"] = cls.Category,
+          ["id"] = cls.ClassID,
+          ["level"] = cls.Level
+        });
+      temp[tostring(cls.PlayPerResetType)] = categoryCount;
+      categoryCount = categoryCount + 1;
+    elseif cls.Category ~= 'None' and result[idx]["level"] > cls.Level then
+      result[idx]["level"] = cls.Level;
+    end
+  end
+
+  return result;
+end
+
 
 function g.processCommand(words)
 	INDUNPLUS_SHOW_PLAYCOUNT();
@@ -77,11 +78,11 @@ function g.getPlayCount(type)
 	return count;
 end
 
-function g.getMaxPlayCount(mission)
-	local pCls = GetClass("Indun", mission);
-	local maxPlayCnt = pCls.PlayPerReset;
+function g.getMaxPlayCount(id)
+	local cls = GetClassByType("Indun", id);
+	local maxPlayCnt = cls.PlayPerReset;
 	if true == session.loginInfo.IsPremiumState(ITEM_TOKEN) then 
-		maxPlayCnt = maxPlayCnt + pCls.PlayPerReset_Token;
+		maxPlayCnt = maxPlayCnt + cls.PlayPerReset_Token;
 	end
 
 	return maxPlayCnt;
@@ -114,8 +115,14 @@ end
 function g.createCharaNameText(frame, cid, record, fontSize, x, y, width, height)
 	local charaText = frame:CreateOrGetControl("richtext", "record"..cid, x, y, width, height)
 	tolua.cast(charaText, "ui::CRichText");
-	
-	local text = string.format("{s%d}%s{/}", fontSize, record.name);
+  local text = "";
+  
+  if record.level == nil then
+    text = string.format("{s%d}%s{/}", fontSize, record.name);
+  else
+    text = string.format("{s%d}Lv%d %s{/}", fontSize, record.level, record.name);
+  end
+
 	charaText:SetText(text);
 	charaText:ShowWindow(1);
 end
@@ -144,6 +151,9 @@ function g.createFBTimeText(frame, cid, record, fontSize, x, y, width, height)
 	fbText:ShowWindow(1);
 end
 
+
+
+
 function INDUNPLUS_TOGGLE_FRAME()
 	local g = _G['ADDONS']['MONOGUSA']['INDUNPLUS'];
 	local acutil = require('acutil');
@@ -160,8 +170,10 @@ function INDUNPLUS_SHOW_PLAYCOUNT()
 	
 	local frame = ui.GetFrame("indunplus");
 	local fontSize = g.settings.fontSize or 16
+  local types = INDUNPLUS_GET_TYPES();
+  local lineNum = #types + 2;
 
-	local topMargin = 15;
+	local topMargin = 30;
 	local bottomMargin = 15;
 	local width = 0;
 	local height = 0;
@@ -173,84 +185,144 @@ function INDUNPLUS_SHOW_PLAYCOUNT()
 	
 	local pageX = 0;
 	local pageY = 15;
-	local pageWidth = 200;
-	local pageHeight = fontSize * 7 + 10;
+	local pageWidth = 250;
+	local pageHeight = fontSize * lineNum + 10;
+  
+  local title = frame:CreateOrGetControl("richtext", "title", 10, 12, pageWidth, fontSize);
+  local closeButton = frame:CreateOrGetControl("button", "close", 0, 0, 25, 25);
+  local minButton = frame:CreateOrGetControl("button", "minimize", 0, 0, 25, 25);
 
-	for cid, record in pairs(records) do
 
-		if cnt > 0 and cnt % rowMax == 0 then
-			row = 0;
-			height = 0;
-			col = col + 1;
-			pageX = pageWidth * col;
-		end
+  if not g.settings.minimize then
+    for cid, record in pairs(records) do
 
-		pageY = pageHeight * row + topMargin;
-		local page = frame:CreateOrGetControl("groupbox", "page_"..cid, pageX , pageY, pageWidth, pageHeight);
-		page:SetSkinName('None');
+      if cnt > 0 and cnt % rowMax == 0 then
+        row = 0;
+        height = 0;
+        col = col + 1;
+        pageX = pageWidth * col;
+      end
 
-		local y = 5;
-		g.createCharaNameText(page, cid, record, fontSize, 12, y, pageWidth, 20);
+      pageY = pageHeight * row + topMargin;
+      local page = frame:CreateOrGetControl("groupbox", "page_"..cid, pageX , pageY, pageWidth, pageHeight);
+      page:SetSkinName('None');
+      page:EnableHitTest(0);
 
-		if nil ~= record.fbDebuffTime and  record.fbDebuffTime > os.time() then
-			y = y + fontSize;
-			g.createFBTimeText(page, cid, record, fontSize, 20, y, pageWidth, 20)
-		end
+      local y = 5;
+      g.createCharaNameText(page, cid, record, fontSize, 12, y, pageWidth, 20);
 
-		for type, counts in pairs(record.counts) do
-			y = y + fontSize;
+      if nil ~= record.fbDebuffTime and  record.fbDebuffTime > os.time() then
+        y = y + fontSize;
+        g.createFBTimeText(page, cid, record, fontSize, 20, y, pageWidth, 20)
+      end
+      
+      for i, indun in ipairs(types) do
+        y = y + fontSize;
+        local counts = record.counts[indun.type];
+        
+        if counts == nil then
+          counts = {
+            playCount = 0,
+            maxPlayCount = g.getMaxPlayCount(indun.id),
+          };
+        end
 
-			local label = g.types[type].label;
+        local label = indun.label;
+        local type = indun.type;
+        local color = "FFFFFF";
+        if record.level ~= nil and indun.level > record.level then
+          color = "444444";
+        elseif counts.playCount >= counts.maxPlayCount then
+          color = "00FF00";
+        elseif counts.playCount > 0 then
+          color = "FFFF00";
+        end
 
-			local color = "FFFFFF";
-			if counts.playCount >= counts.maxPlayCount then
-				color = "00FF00";
-			elseif counts.playCount > 0 then
-				color = "FFFF00";
-			end
+        local labelText = page:CreateOrGetControl("richtext", "label"..cid.."_"..type, 20, y, pageWidth/2, 15);
+        tolua.cast(labelText, "ui::CRichText");
+        labelText:SetText( string.format("{#%s}{s%d}%s{/}{/}", color, fontSize ,label));
 
-			local labelText = page:CreateOrGetControl("richtext", "label"..cid.."_"..type, 20, y, pageWidth/2, 15);
-			tolua.cast(labelText, "ui::CRichText");
-			labelText:SetText( string.format("{#%s}{s%d}%s{/}{/}", color, fontSize ,label));
-			labelText:ShowWindow(1);
+        local countText = page:CreateOrGetControl("richtext", "count"..cid.."_"..type, 0, y, pageWidth/2, 15);
+        tolua.cast(countText, "ui::CRichText");
+        countText:SetText( string.format("{#%s}{s%d}%d/%d{/}{/}", color, fontSize, counts.playCount, counts.maxPlayCount));
+        countText:SetGravity(1,0);
+      end
+      
+      page:Resize(pageWidth, pageHeight);
 
-			local countText = page:CreateOrGetControl("richtext", "count"..cid.."_"..type, 0, y, pageWidth/2, 15);
-			tolua.cast(countText, "ui::CRichText");
-			countText:SetText( string.format("{#%s}{s%d}%d/%d{/}{/}", color, fontSize, counts.playCount, counts.maxPlayCount));
-			countText:SetGravity(1,0);
-			countText:ShowWindow(1);
-		end
+      if cnt < rowMax then
+        height = pageHeight * (row + 1) + bottomMargin + topMargin;
+      else
+        height = pageHeight * rowMax + bottomMargin + topMargin;
+      end
 
-		page:Resize(pageWidth, pageHeight);
+      cnt = cnt + 1;
+      row = row + 1;
+    end
+    closeButton:Move(0, 0);
+    minButton:Move(0, 0);
+    closeButton:SetOffset(pageWidth * (col + 1) -25, 10);
+    minButton:SetOffset(pageWidth * (col + 1) -50, 10);
+    frame:Resize(pageWidth * (col + 1) + 10, height);
+  else
+    closeButton:Move(0, 0);
+    minButton:Move(0, 0);
+    closeButton:SetOffset(200 -25, 5);
+    minButton:SetOffset(200 -50, 5);
+    frame:Resize(200, 35);
+  end
 
-		if cnt < rowMax then
-			height = pageHeight * (row + 1) + bottomMargin + topMargin;
-		else
-			height = pageHeight * rowMax + bottomMargin + topMargin;
-		end
+	frame:Move(0, 0);
+	frame:SetOffset(g.settings.xPosition, g.settings.yPosition);
+end
 
-		cnt = cnt + 1;
-		row = row + 1;
-	end
-	frame:Resize(pageWidth * (col + 1) + 10, height);
+function INDUNPLUS_MINIMIZE_FRAME()
+	local frame = ui.GetFrame("indunplus");
+	g.settings.xPosition = frame:GetX();
+	g.settings.yPosition = frame:GetY();
+	g.settings.minimize = not g.settings.minimize;
 
+ 	acutil.saveJSON(g.settingsFileLoc, g.settings);
+  INDUNPLUS_SHOW_PLAYCOUNT();
 end
 
 function INDUNPLUS_ON_INIT(addon, frame)
 	local g = _G['ADDONS']['MONOGUSA']['INDUNPLUS'];
-	local acutil = require('acutil');
 
 	g.addon = addon;
 	g.frame = frame;
 	frame:ShowWindow(0);
+
+	frame:EnableHitTest(1);
+	frame:SetEventScript(ui.RBUTTONDOWN, "INDUNPLUS_CONTEXT_MENU");
+
 	frame:SetEventScript(ui.LBUTTONDOWN, "INDUNPLUS_START_DRAG");
 	frame:SetEventScript(ui.LBUTTONUP, "INDUNPLUS_END_DRAG");
 	g.addon:RegisterMsg("GAME_START_3SEC", "INDUNPLUS_3SEC");
 	g.addon:RegisterMsg('BUFF_ADD', 'INDUNPLUS_UPDATE_BUFF');
 	g.addon:RegisterMsg('BUFF_REMOVE', 'INDUNPLUS_UPDATE_BUFF');
 
+  local title = frame:CreateOrGetControl("richtext", "title", 10, 12, 200, 16);
+  title:EnableHitTest(0);
+  tolua.cast(title, "ui::CRichText");
+  title:SetText("IndunPlus /idp");
+  
+  local closeButton = frame:CreateOrGetControl("button", "close", 0, 0, 25, 25);
+  closeButton:SetEventScript(ui.LBUTTONDOWN, "INDUNPLUS_TOGGLE_FRAME");
+  closeButton:SetText("X");
+
+  local minButton = frame:CreateOrGetControl("button", "minimize", 0, 0, 25, 25);
+  minButton:SetEventScript(ui.LBUTTONDOWN, "INDUNPLUS_MINIMIZE_FRAME");
+  minButton:SetText("_");
 end
 
+function INDUNPLUS_CONTEXT_MENU()
+	local context = ui.CreateContextMenu("INDUNPLUS_RBTN", "IndunPlus", 0, 0, 300, 100);
+	ui.AddContextMenuItem(context, "Hide (/idp)", "INDUNPLUS_TOGGLE_FRAME()");
+  ui.AddContextMenuItem(context, "Toggle Minimize", "INDUNPLUS_MINIMIZE_FRAME()");
+	context:Resize(300, context:GetHeight());
+	ui.OpenContextMenu(context);
+end
 
 function INDUNPLUS_UPDATE_BUFF(frame, msg, argStr, argNum)
 	local g = _G['ADDONS']['MONOGUSA']['INDUNPLUS'];
@@ -289,13 +361,10 @@ function INDUNPLUS_CHECK_BUFF()
 	
 	for i = 0, buffCount - 1 do
 		local buff = info.GetBuffIndexed(handle, i);
-		local class = GetClassByType('Buff', buff.buffID);
-		--CHAT_SYSTEM(class.ClassName.."="..buff.buffID.." time:"..buff.time.."ms");
 		
 		if buff.buffID == g.bossDebuffId then
 			fbDebuff = true;
 			fbTime = os.time() + math.floor(buff.time / 1000);
-			local fbDate = os.date("*t", fbTime);
 		end
 	end
 	
@@ -361,8 +430,10 @@ function INDUNPLUS_SAVE_TIME()
 	local cid = mySession:GetCID();
 	local charName = info.GetName(session.GetMyHandle());
 	local time = os.time();
+  local level = info.GetLevel(session.GetMyHandle());
 
 	g.settings.records[cid] = {
+    ["level"] = level,
 		["name"] = charName,
 		["time"] = time,
 		["counts"] = {},
@@ -372,11 +443,11 @@ function INDUNPLUS_SAVE_TIME()
 
 	local counts = g.settings.records[cid]["counts"];
 
-	for type, mission in pairs(g.types) do
-		local playCount = g.getPlayCount(type);
-		local maxPlayCount =  g.getMaxPlayCount(mission.id);
+	for i, indun in ipairs(INDUNPLUS_GET_TYPES()) do
+		local playCount = g.getPlayCount(indun.type);
+		local maxPlayCount =  g.getMaxPlayCount(indun.id);
 
-		counts[type] = {
+		counts[indun.type] = {
 			["playCount"] = playCount,
 			["maxPlayCount"] = maxPlayCount,
 		};
@@ -387,7 +458,6 @@ end
 
 function INDUNPLUS_REFLESH_COUNTS()
 	local g = _G['ADDONS']['MONOGUSA']['INDUNPLUS'];
-	local acutil = require('acutil');
 	local resetTime = g.getResetTime();
 	local records = g.settings.records;
 	
@@ -396,12 +466,12 @@ function INDUNPLUS_REFLESH_COUNTS()
 
 			local counts = record.counts;
 
-			for type, mission in pairs(g.types) do
+			for i, indun in ipairs(INDUNPLUS_GET_TYPES()) do
 				local playCount = 0;
-				local maxPlayCount =  g.getMaxPlayCount(mission.id);
+				local maxPlayCount =  g.getMaxPlayCount(indun.id);
 	
-				counts[type]["playCount"] = playCount;
-				counts[type]["maxPlayCount"] = maxPlayCount;
+				counts[indun.type]["playCount"] = playCount;
+				counts[indun.type]["maxPlayCount"] = maxPlayCount;
 			end
 		end
 	end
